168397cf7132087b5766229b0ed369cf94ba731f
Belay Alamnehe <semrebelay@gmail.com>
2023-11-04 00:10:01 -0700

    style the active link in the navBar

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,8 +1,11 @@
-import link from 'next/link'
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug }  from 'react-icons/ai'
+import classnames from 'classnames'
 const NavBar = () => {
+    const currentPath= usePathname();
     const links=[
         {label:'Dashboard', href:'/'},
         {label:'Users', href:'/users'},
@@ -15,7 +18,15 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-10 items-center'>
         <Link href="/"><AiFillBug /> </Link>
         <ul className='flex space-x-4'>
-            {links.map(link=><Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-color' href="/">{link.label} </Link>)};
+            {links.map(link=>
+            <Link
+             key={link.href} 
+             className= {classnames({
+                'text-zinc-1000':link.href===currentPath,
+                'text-zinc-500':link.href!==currentPath,
+                'hover:text-zinc-800 transion-colots':true  
+             })}
+             href={link.href}>{link.label} </Link>)}
            
            </ul>
 
---
+++ app/sectors/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectorPage = () => {
+  return (
+    <div>SectorPage</div>
+  )
+}
+
+export default SectorPage
