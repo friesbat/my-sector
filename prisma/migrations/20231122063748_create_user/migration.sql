bd4eadb8af5011a22fe69dceb70fb9176059e457
Belay Alamnehe <semrebelay@gmail.com>
2023-11-21 22:38:58 -0800

    create the user model

---
+++ prisma/migrations/20231122063748_create_user/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `full_name` VARCHAR(255) NOT NULL,
+    `eamil` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(255) NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `upadatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
